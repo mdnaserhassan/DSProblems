ec0670e616d6d479a24a960a8f4b576f8cb8ef2f
unknown <mdnaserhassan@gmail.com>
2016-06-24 05:23:21 +0530

    separted stack class into new file

--- BrackProblem/BrackProblem/Program.cs
+++ BrackProblem/BrackProblem/Program.cs
@@ -6,56 +6,7 @@ using System.Threading.Tasks;
 
 namespace BrackProblem
 {
-    class StackProblems
-    {
-        /// <summary>
-        /// This Method returns true if for all open brackets in a string there is closed bracket
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public  int CheckBracket(string input)
-        {
-            char[] inputs = input.ToArray();
-            Stack<char> openBracket = new Stack<char>();
-            for (int i = 0; i < inputs.Length; i++)
-            {
-                if (inputs[i] == '(' || inputs[i] == '{' || inputs[i] == '[')
-                {
-                    openBracket.Push(inputs[i]);
-                }
-                if (inputs[i] == ')' || inputs[i] == '}' || inputs[i] == ']')
-                {
-                    char inm = openBracket.Pop();
-                    if (inputs[i] == ')' && inm == '(')
-                    {
-
-                    }
-                    else if (inputs[i] == '}' && inm == '{')
-                    {
-
-                    }
-                    else if (inputs[i] == ']' && inm == '[')
-                    {
-
-                    }
-                    else
-                    {
-                        break;
-                    }
-
-
-                }
-            }
-            return openBracket.Count;
-
-
-
-
-
-
-
-        }
-    }
+    
     class Program
     {
         static void Main(string[] args)
