05a40b52e041db78e6795c838b20ec285dce14d7
unknown <mdnaserhassan@gmail.com>
2016-06-24 03:24:56 +0530

    First commit

---
+++ BrackProblem/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrackProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "[[()]{}]";
+           int count= CheckBracket(input);
+           if (count > 0)
+           {
+               Console.WriteLine("False");
+           }
+           else
+           {
+               Console.WriteLine("True");
+           }
+            Console.ReadLine();
+        }
+
+        private static int CheckBracket(string input)
+        {
+            char[] inputs = input.ToArray();
+            Stack<char> openBracket = new Stack<char>();
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                if (inputs[i] == '(' || inputs[i] == '{' || inputs[i] == '[')
+                {
+                    openBracket.Push(inputs[i]);
+                }
+                if (inputs[i] == ')' || inputs[i] == '}' || inputs[i] == ']')
+                {
+                    char inm = openBracket.Pop();
+                    if (inputs[i] == ')' && inm == '(')
+                    {
+
+                    }
+                    else if (inputs[i] == '}' && inm == '{')
+                    {
+
+                    }
+                    else if (inputs[i] == ']' && inm == '[')
+                    {
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+
+                }
+            }
+            return openBracket.Count;
+
+
+
+
+
+
+
+        }
+    }
+}
