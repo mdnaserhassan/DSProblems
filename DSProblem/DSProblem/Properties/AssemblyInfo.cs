f7ba978e3ca4f4b64b467b244ba66e4804249193
unknown <mdnaserhassan@gmail.com>
2016-09-08 03:07:06 +0530

    Name change

--- DSProblem/DSProblem/Properties/AssemblyInfo.cs
+++ DSProblem/DSProblem/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BrackProblem")]
+[assembly: AssemblyTitle("DSProblem")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BrackProblem")]
+[assembly: AssemblyProduct("DSProblem")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
