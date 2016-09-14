cb8f063192e9af57d98b6bb0f5d801d76e401798
Vivek <Vivek14june@gmail.com>
2016-09-15 04:22:31 +0530

    Array Majority problem

--- DSProblem/DSProblem/ArrayProblem.cs
+++ DSProblem/DSProblem/ArrayProblem.cs
@@ -8,5 +8,34 @@ namespace DSProblem
 {
     class ArrayProblem
     {
+
+        public int FindMajority(int[] input)
+        {
+            int length = input.Length;
+            int atmost = (length / 2) + 1;
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                int count = 1;    
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (input[i]==input[j])
+                    {
+                        count++;
+                    }
+                    
+                    if (atmost==count)
+                    {
+                        return input[i];
+                    }
+
+                   
+                }
+                Console.WriteLine(count);
+             
+            }
+            
+            return -1;
+        }
     }
 }
--- DSProblem/DSProblem/Program.cs
+++ DSProblem/DSProblem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DSProblem;
 
 namespace BrackProblem
 {
@@ -22,17 +23,21 @@ namespace BrackProblem
             //{
             //    Console.WriteLine("True");
             //}
-            string check = "e";
-            while (check != "q")
-            {
-                StringHelper shelp = new StringHelper();
-                Console.WriteLine("Enter Input to test");
-                string input2 = Console.ReadLine();
+            //string check = "e";
+            //while (check != "q")
+            //{
+            //    StringHelper shelp = new StringHelper();
+            //    Console.WriteLine("Enter Input to test");
+            //    string input5 = Console.ReadLine();
                 
-                Console.WriteLine("Is the input can be palindrom ? {0}", shelp.CanBePalindrom(input2));
-                Console.WriteLine("Enter q to exit anyother character to continue for other input");
-                check = Console.ReadLine();
-            }
+            //    Console.WriteLine("Is the input can be palindrom ? {0}", shelp.CanBePalindrom(input5));
+            //    Console.WriteLine("Enter q to exit anyother character to continue for other input");
+            //    check = Console.ReadLine();
+            //}
+            ArrayProblem arr = new ArrayProblem();
+            int[] arr5 = new int[] { 1, 5, 1, 1, 3, 1, 5, 1, 1, 5 };
+            
+            int i=arr.FindMajority(new int[]{1,2,1,2,1,2,1,2,3,4,4,5,6,1,1,1,1,1 });
 
 
 
